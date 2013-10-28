7a73a124062c54036de6ce926a205d20111d9afa
As <andrewshield99@hotmail.com>
2013-10-28 21:03:03 +1300

    Final
    Final to hand in. Fully working (no performance optimisations or
    validation)

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
   $(document).ready(function() {
   $('#item').focus();
   $('#add').click(submit);
@@ -9,6 +8,7 @@
     });
 // Re-order the list
 $("#shopping-list").sortable();
+
 }); // End of document ready
 
 var item = {}
@@ -17,26 +17,20 @@ var onList = [];
 // Submitting user input
 function submit(){
   var list = document.getElementById("item").value;
-  strikeThrough();
   validation (list)
 // Reset input after submit   
     $("#item").val('');
 
 // Validating user input  
 function validation (list){
-  if (list == "" || list == " ") {
+  var reg = /^\s+|\s+$/g;  // Will reject trailing and leading spaces
+  if (reg.test(list) || list == "") {
     shake();
     return;
   }
   else addItem();
 }
-function shake(){
-    $("#item").addClass('shake animated').delay(1000).queue(function(next){
-    $(this).removeClass('shake animated');
-    next();
-    });
-  };
-  
+ 
 // Adding items to the array
   function addItem() {
     onList.push(list);{
@@ -45,21 +39,30 @@ function shake(){
     onList[finalNum];
     var newListItem = "<li>" + "<input class='checkbox' type='checkbox'>" + "<span>"  + onList[finalNum] + "</span>" + "</li>";
     $(newListItem).appendTo(".shopping-list").hide().slideDown("fast");
-
+    };
+    removeInstructions()
   };
-  };
 
-// Removing items from the list / array
-$("#subtract").click(function(){
-$(".shopping-list input:checked").each(function() {
-    var index = $.trim($(".shopping-list input:checkbox").index(this));
-    onList.splice(index);
-    $(".shopping-list li:eq(" + index + ")").slideUp("fast")
-  });
-});
-
+strikeThrough();
 
 }; // End of submit function
+
+removeItem();
+
+// Validation animation
+function shake(){
+    $("#item").addClass('shake animated').delay(1000).queue(function(next){
+    $(this).removeClass('shake animated');
+    next();
+    });
+  };
+
+// Remove initial instructions 
+function removeInstructions() {
+ if (onList.length > 0) {
+  $('.instructions').remove()
+  };
+}
 
 // Crossing items off
 function strikeThrough() {
@@ -69,9 +72,20 @@ function strikeThrough() {
     }
   else 
       $(this).parent().removeClass('checked-off'); 
-
   });
 }
+
+// Removing items from the list / array
+function removeItem() {
+$("#subtract").click(function(){
+$(".shopping-list input:checked").each(function() {
+    var index = $.trim($(".shopping-list input:checkbox").index(this));
+    onList.splice(index);
+    $(".shopping-list li:eq(" + index + ")").slideUp("fast")
+  });
+});
+}
+
 
 
 
